cda9e44042c47044dd06c31804e9ad8be95399fe
Andrew Morgan <andrew.morgan@opencredo.com>
2016-03-07 12:41:17 +0000

    Test for rule where a URL is configured as the source json

---
+++ src/test/java/URLHoverflyRuleTest.java
@@ -0,0 +1,61 @@
+import io.specto.hoverfly.junit.HoverflyRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+public class URLHoverflyRuleTest {
+
+    @Rule
+    public HoverflyRule hoverflyRule = HoverflyRule.buildFromUrl("http://raw.githubusercontent.com/SpectoLabs/hoverfly-junit/d0d41dfdcb250c6bb02ada63d304b4afddf5f2e4/src/test/resources/test-service.json").build();
+
+    private RestTemplate restTemplate;
+
+    @Before
+    public void setUp() {
+        restTemplate = new RestTemplate();
+    }
+
+    @Test
+    public void shouldBeAbleToMakeABookingUsingHoverfly() throws URISyntaxException {
+        // Given
+        final RequestEntity<String> bookFlightRequest = RequestEntity.post(new URI("http://www.my-test.com/api/bookings"))
+                .contentType(APPLICATION_JSON)
+                .body("{\"flightId\": \"1\"}");
+
+        // When
+        final ResponseEntity<String> bookFlightResponse = restTemplate.exchange(bookFlightRequest, String.class);
+
+        // Then
+        assertThat(bookFlightResponse.getStatusCode()).isEqualTo(CREATED);
+        assertThat(bookFlightResponse.getHeaders().getLocation()).isEqualTo(new URI("http://localhost/api/bookings/1"));
+    }
+
+    @Test
+    public void shouldBeAbleToGetABookingUsingHoverfly() {
+        // When
+        final ResponseEntity<String> getBookingResponse = restTemplate.getForEntity("http://www.my-test.com/api/bookings/1", String.class);
+
+        // Then
+        assertThat(getBookingResponse.getStatusCode()).isEqualTo(OK);
+        assertThatJson(getBookingResponse.getBody()).isEqualTo("{" +
+                "\"bookingId\":\"1\"," +
+                "\"origin\":\"London\"," +
+                "\"destination\":\"Singapore\"," +
+                "\"time\":\"2011-09-01T12:30\"," +
+                "\"_links\":{\"self\":{\"href\":\"http://localhost/api/bookings/1\"}}" +
+                "}");
+    }
+
+}
