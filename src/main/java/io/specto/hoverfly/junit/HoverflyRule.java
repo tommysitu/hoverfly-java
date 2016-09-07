309c4c7462ec1e0f828632ea1ed23ec7afef6e73
Andrew Morgan <andrew.morgan@opencredo.com>
2016-09-07 11:50:32 +0100

    Changed access modifiers of builders so they can be used outside of tests

--- src/main/java/io/specto/hoverfly/junit/HoverflyRule.java
+++ src/main/java/io/specto/hoverfly/junit/HoverflyRule.java
@@ -1,3 +1,15 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * <p>
+ * Copyright 2016-2016 SpectoLabs Ltd.
+ */
 /*
   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
   the License. You may obtain a copy of the License at
@@ -71,7 +83,7 @@ public class HoverflyRule extends ExternalResource {
         serviceDataURI = dataResourcePath.toString();
     }
 
-    private HoverflyRule(final int proxyPort, final int adminPort, final HoverflyMode hoverflyMode, final boolean proxyLocalHost) {
+    HoverflyRule(final int proxyPort, final int adminPort, final HoverflyMode hoverflyMode, final boolean proxyLocalHost) {
         this.proxyPort = proxyPort == 0 ? findUnusedPort() : proxyPort;
         this.adminPort = adminPort == 0 ? findUnusedPort() : adminPort;
         this.hoverflyMode = hoverflyMode;
